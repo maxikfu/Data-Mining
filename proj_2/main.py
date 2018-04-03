c9e3b4abc88ef91c10ea7276a151cbea4cc39761
Maksim Egorov <megorov@uh.edu>
2018-04-02 22:10:46 -0500

    proper input and output

--- proj_2/main.py
+++ proj_2/main.py
@@ -89,16 +89,36 @@ def main(query_type,query_string,output_file_path):
 
 if __name__ == "__main__":#reading data from cmd line
 	if len(sys.argv) > 2:
-		query_type,query_string,output_file_path = main(sys.argv[1],sys.argv[2],sys.argv[3])
+		query_type,query_list,output_file_path = main(sys.argv[1],sys.argv[2:-1],sys.argv[-1])
 
-# orig_stdout = sys.stdout #output in the file
-# fout = open(output_file_path, 'w')
-# sys.stdout = fout
-#global total_doc_id
-
+query_string = ' '.join(query_list)
+orig_stdout = sys.stdout #output in the file
+fout = open(output_file_path, 'w')
+sys.stdout = fout
+lower_case_query = ''
+word_a = None
+for i in query_list:
+	if i not in ['(',')','AND','OR','NOT']:
+		if word_a != None:
+			word_b = i.lower()
+			lower_case_query=lower_case_query+' '+word_b
+		else:
+			word_a = i.lower()
+			lower_case_query=lower_case_query+' '+word_a
+	else:
+		lower_case_query=lower_case_query+' '+i
 vocab_map_file_path = 'proj_2/vocab_map.txt'
 doc_file_path = 'proj_2/docs.txt'
 
 posting_list, vocab_map,doc_vocab,total_doc_id = create_posting_list(vocab_map_file_path,doc_file_path)
 
-print(sorted(evaluateAND_NOTQuery(posting_list['scroll'],posting_list['mouse'])))
+if query_type == 'PLIST':
+	print(lower_case_query.strip(),'->',sorted(posting_list[lower_case_query]))
+elif (query_type == 'AND'):
+	print(lower_case_query.strip(),'->',sorted(evaluateANDQuery(posting_list[word_a],posting_list[word_b])))
+elif (query_type == 'OR'):
+	print(lower_case_query.strip(),'->',sorted(evaluateORQuery(posting_list[word_a],posting_list[word_b])))
+else:
+	print(lower_case_query.strip(),'->',sorted(evaluateAND_NOTQuery(posting_list[word_a],posting_list[word_b])))
+
+
